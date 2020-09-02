298c95035036d6fbf122890f24a70af6d6c260f0
yushinbox@gmail.com <yushinbox@gmail.com>
2020-09-02 15:15:06 +0300

    занес в work пример длля миксина

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
-import './js/jquery'
+//------------- это подключает jquery------
+import $ from 'jquery';
+// ------------------------------------
+// ----------это подключает все стили и в этом файле есть scss bootstrap из node modules------
+import './scss/style.scss';
+// ----------------------------------------------
+
+
+
+
+import './js/common.js'
+// import './css/main.css';
 // import "./js/bootstrap.bundle";
 // import "./js/bootstrap";
-// import $ from 'jquery';
-// import './css/main.css';
-import './css/bootstrap-grid.css';
-import './scss/style.scss';
+// import './css/bootstrap-grid.css';
+// import './css/bootstrap.css';
+// import 'node_modules/bootstrap/scss/bootstrap';
+// import "node_modules/bootstrap/scss/grid";
 // import "slick-carousel"; 
 // import "mixitup";
 // import "./js/sliMy.js";
@@ -14,234 +25,10 @@ import './scss/style.scss';
 // alert("ppppp"); 
 
 
-// $('.title').html('какой-то текст')
-// $('.title').addClass('.class-new')
+// проверка присутствия jquery
+$(document).ready(function () {
+  $(".title").css({'color': '#9fff0f', 'font-size':'20px','text-align':'center','width':'100%', 'padding':'20px', 'background':'purple'});
+  
+  $(".title").html("если виден текст, то на сайте работает jquery потому что  сам div и свойства данного div  зашли через js");
 
-
-// // -------------burger----------------------
-// $(Document).ready(function () {
-//   $(".icon-menu").click(function (event) {
-//     $(this).toggleClass("active"),
-//     $(".menu").toggleClass("active");
-//     $(".header-tel").toggleClass("active");
-
-//     $("body").toggleClass("lock");
-//   });
-// });
-
-
-
-
-
-// -------------burger-slide----------------------
-$(Document).ready(function () {
-  $(".icon-menu-1").click(function (event) {
-    $(this).toggleClass(".icon-menu-1-active"),
-      $(".header__slide").toggleClass("active");
-  });
-});
-$(Document).ready(function () {
-  $(".icon-menu-1-1").click(function (event) {
-    $(this).toggleClass(".icon-menu-1-active"),
-      $(".header1-1__slide").toggleClass("active");
-  });
-});
-//--------- .projects__button
-$(Document).ready(function () {
-  $(".projects__button").click(function (event) {
-      $(".projects-galery-hidden").toggleClass(
-        "projects-galery-hidden_active"
-      );
-  });
-});
-
-
-
-
-// ==========ibg=========
-function ibg() {
-  $.each($('.ibg'), function (index, val) {
-    if ($(this).find('img').length > 0) {
-      $(this).css('background-image', 'url("' + $(this).find('img').attr('src') + '")');
-    }
-  });
-}
-
-ibg();
-
-
-
-
-$(Document).ready(function () {
-  $(".slider-des-js").slick({
-    arrows: true,
-    // dots: true,
-    slidesToShow: 1,
-    // infinite: false,
-    slideToscroll: 1,
-    speed: 800,
-    // easing: "ease",
-    // adapitveHeight: true,
-    rows: 1,
-    nextArrow: $(".arrows-slider-des__arrow_prev"),
-    prevArrow: $(".arrows-slider-des__arrow_next"),
-    responsive: [
-      {
-        breakpoint: 960,
-        settings: {
-          // arrows: false,
-          slidesToShow: 1,
-        },
-      },
-    ],
-  });
-});
-
-
- 
-$(Document).ready(function () {
-  $(".slider-invitation-js").slick({
-    arrows: true,
-    dots: true,
-    slidesToShow: 1,
-    // infinite: false,
-    slideToscroll: 1,
-    speed: 800,
-    easing: "ease",
-    adapitveHeight: true,
-    rows: 1,
-    nextArrow: $(".arrows-slider-invitation__arrow_prev"),
-    prevArrow: $(".arrows-slider-invitation__arrow_next"),
-    // responsive: [
-    //   {
-    //     breakpoint: 960,
-    //     settings: {
-    //       // arrows: false,
-    //       slidesToShow: 1,
-    //     },
-    //   },
-    // ],
-  });
-});
-
-
-$(Document).ready(function () {
-  $(".slider-confirm-js").slick({
-    arrows: true,
-    dots: true,
-    slidesToShow: 1,
-    // infinite: false,
-    slideToscroll: 1,
-    speed: 800,
-    easing: "ease",
-    adapitveHeight: true,
-    rows: 1,
-    nextArrow: $(".arrows-slider-confirm__arrow_prev"),
-    prevArrow: $(".arrows-slider-confirm__arrow_next"),
-    // responsive: [
-    //   {
-    //     breakpoint: 960,
-    //     settings: {
-    //       // arrows: false,
-    //       slidesToShow: 1,
-    //     },
-    //   },
-    // ],
-  });
-});
-
-
-$(Document).ready(function () {
-  $(".slider-work-js").slick({
-    arrows: true,
-    dots: true,
-    slidesToShow: 1,
-    // infinite: false,
-    slideToscroll: 1,
-    speed: 800,
-    easing: "ease",
-    adapitveHeight: true,
-    rows: 1,
-    nextArrow: $(".arrows-slider-work__arrow_prev"),
-    prevArrow: $(".arrows-slider-work__arrow_next"),
-    // responsive: [
-    //   {
-    //     breakpoint: 960,
-    //     settings: {
-    //       // arrows: false,
-    //       slidesToShow: 1,
-    //     },
-    //   },
-    // ],
-  });
-});
-$(Document).ready(function () {
-  $(".gs-js").slick({
-    arrows: true,
-    // dots: true,
-    slidesToShow: 3,
-    // infinite: false,
-    slideToscroll: 1,
-    speed: 800,
-    easing: "ease",
-    // adapitveHeight: true,
-    rows: 1,
-    nextArrow: $(".gs-js_prev"),
-    prevArrow: $(".gs-js_next"),
-    responsive: [
-      {
-        breakpoint: 700,
-        settings: {
-          // arrows: false,
-          slidesToShow: 1,
-        },
-      },
-    ],
-  });
-});
-
-
-// ------------------------------
-
- $(function () {
-   $("#Container").mixItUp({
-     selectors: {
-       target: ".mix",
-       filter: ".filter",
-       sort: ".sort",
-     },
-     load: {
-       filter: ".category-1",
-       sort: "myorder:asc",
-     },
-
-     controls: {
-       enable: true,
-       //activeClass:'on'
-     },
-
-     animation: {
-       enable: true,
-       effects: "scale fade",
-       duration: 500,
-     },
-
-     /*layout: {
-			//display:'block'
-			containerClass:'list'
-		}*/
-
-     callbacks: {
-       /*onMixLoad:function(state) {
-				alert('Load');
-			},*/
-       // onMixStart: function (state) {
-       //   alert(state.$targets.text());
-       // },
-       /*onMixEnd:function() {
-				alert('End');
-			}*/
-     },
-   });
  });
-
