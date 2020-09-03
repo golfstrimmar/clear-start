5a33a49b3af86bda0f040f5a78e4cfd1318b1d01
yushinbox@gmail.com <yushinbox@gmail.com>
2020-09-03 15:57:55 +0300

    03,09

--- src/js/common.js
+++ src/js/common.js
@@ -2,3 +2,13 @@
 import $ from "jquery";
 // ------------------------------------------------
 
+// ==========ibg=========
+function ibg() {
+  $.each($('.ibg'), function (index, val) {
+    if ($(this).find('img').length > 0) {
+      $(this).css('background-image', 'url("' + $(this).find('img').attr('src') + '")');
+    }
+  });
+}
+
+ibg();
